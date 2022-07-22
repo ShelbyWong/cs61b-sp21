4bf26e67c1189ccb44a107f1bf7d7b3c3c2c3a7d
ShelbyW <76605025+ShelbyWong@users.noreply.github.com>
2022-07-22 17:20:17 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -9,9 +9,17 @@ public class Collatz {
      * @return  return a nextNumber
      */
     public static int nextNumber(int n) {
-        if(n==1)return n;
-        if(n%2==0) return n/2;
-        else return 3*n+1;
+        //buggy version
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
+//        if(n==1)return n;
+//        if(n%2==0) return n/2;
+//        else return 3*n+1;
     }
 
 
