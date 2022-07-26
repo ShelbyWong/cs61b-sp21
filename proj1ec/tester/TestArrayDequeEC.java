1ddaa5ab9375f95c8776afeb0256cb97ac1c5b2b
ShelbyW <76605025+ShelbyWong@users.noreply.github.com>
2022-07-26 20:37:59 +0800

    t

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,28 +12,29 @@ public class TestArrayDequeEC {
     @Test
     public void test1(){
         StudentArrayDeque<Integer> sad =  new StudentArrayDeque<>();
-        for (int i = 0; i < 1000; i++) {
-            double uniform = i*StdRandom.uniform();
 
-            if ((int)uniform<i/2){
-                sad.addLast((int)uniform);
-            }else {
-                sad.addFirst((int)uniform);
-            }
-        }
-        sad.printDeque();
+        int expected = 20;
+        sad.addFirst(expected);
+        int actual = StdRandom.uniform(6);
+
+        assertEquals("addFirst("+expected+")"+'\n'+
+                        "removeFirst()"+'\n',
+                expected,
+                actual);
     }
 
     @Test
     public void test2(){
         StudentArrayDeque<Integer> sad =  new StudentArrayDeque<>();
-        for (int i = 1; i < 100; i++) {
-            int expected =StdRandom.uniform(i);
-            sad.addFirst(expected);
+        for (int i = 1; i < 10000; i++) {
+            int expected1 =StdRandom.uniform(i);
+            sad.addFirst(expected1);
+            int expected2 =StdRandom.uniform(i);
+            sad.addFirst(expected2);
             int actual = sad.removeFirst();
-            assertEquals("addFirst("+expected+")"+'\n'+
+            assertEquals("addFirst("+expected1+")"+'\n'+
                             "removeFirst()"+'\n',
-                                expected,
+                                expected1,
                                 actual);
         }
 
