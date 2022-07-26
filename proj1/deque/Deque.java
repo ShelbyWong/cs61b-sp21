41f0fcc3f0e1c4713538923abcace6aa39a31ebe
ShelbyW <76605025+ShelbyWong@users.noreply.github.com>
2022-07-26 21:00:04 +0800

    t

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -1,6 +1,5 @@
 package deque;
 
-import java.util.Iterator;
 
 public interface Deque<T> {
     void printDeque();
@@ -11,6 +10,4 @@ public interface Deque<T> {
     T removeFirst();
     T removeLast();
     T get(int index);
-    //Iterator<T> iterator();
-    boolean equals(Object o);
 }
